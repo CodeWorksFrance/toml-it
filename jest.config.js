93e00ba132fd6115194c7c308c2fff921452080c
Trisralei <timothee.arnauld1@gmail.com>
2022-11-08 19:21:23 +0100

    :wrench: add prebuild step for coverage ratio checkup

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    }
+  }
 };
