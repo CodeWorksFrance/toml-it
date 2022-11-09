34c6a560819c8d67b8a055d269a861b0fd218a56
Trisralei <timothee.arnauld1@gmail.com>
2022-11-09 16:27:47 +0100

    :rotating_light: add eslint and `npm run lint` command

--- src/framework.spec.ts
+++ src/framework.spec.ts
@@ -1,54 +1,56 @@
-import * as glob from "glob";
-import * as fs from "fs";
-import { TestRunner } from "./toml";
+import * as glob from 'glob';
+import * as fs from 'fs';
+import { TestRunner } from './toml';
 
-describe("Framework specifications", () => {
-  describe("TOML parsing", () => {
-    it("should parse test file", () => {
-      jest.spyOn(fs, "readFileSync").mockReturnValueOnce(`
+describe('Framework specifications', () => {
+  describe('TOML parsing', () => {
+    it('should parse test file', () => {
+      jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(`
         stdout="test"
         args="argument_1 argument_2 argument_3"
         description="this is a correct toml test description"`);
       new TestRunner().getTestsStructures();
     });
 
-    it("should throw an exception when properties length is not correct", () => {
-      jest.spyOn(fs, "readFileSync").mockReturnValueOnce(`
+    it('should throw an exception when properties length is not correct', () => {
+      jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(`
         stdout="test"
         args="argument_1 argument_2 argument_3"
         description="this is my first test"
         unknowProperty=""`);
       try {
         new TestRunner().getTestsStructures();
-      } catch (e: any) {
-        expect(e.message).toEqual(
-          "There are some missing properties in the TOML file."
+      } catch (e) {
+        expect((e as { message: string }).message).toEqual(
+          'There are some missing properties in the TOML file.'
         );
       }
     });
 
-    it("should throw an exception when properties is not correct", () => {
+    it('should throw an exception when properties is not correct', () => {
       jest
-        .spyOn(fs, "readFileSync")
+        .spyOn(fs, 'readFileSync')
         .mockReturnValueOnce('unknowProperty="test" \n\t');
 
       try {
         new TestRunner().getTestsStructures();
-      } catch (e: any) {
-        expect(e.message).toEqual("Properties are probably mispelled.");
+      } catch (e) {
+        expect((e as { message: string }).message).toEqual(
+          'Properties are probably mispelled.'
+        );
       }
     });
   });
 
-  describe("Run", () => {
+  describe('Run', () => {
     beforeEach(() => {
       jest.clearAllMocks();
       jest.useFakeTimers();
     });
 
-    it("should display correct result with success status", () => {
-      jest.spyOn(glob, "sync").mockReturnValueOnce(["test.spec.toml"]);
-      jest.spyOn(fs, "readFileSync")
+    it('should display correct result with success status', () => {
+      jest.spyOn(glob, 'sync').mockReturnValueOnce(['test.spec.toml']);
+      jest.spyOn(fs, 'readFileSync')
         .mockReturnValueOnce(`stdout = "not ok\nhello world!\nGoodbye Marty!"
       args = "argument_1 argument_2 argument_3"
       description="Should display 'not ok'"
@@ -56,17 +58,19 @@ describe("Framework specifications", () => {
       expect(new TestRunner().run()).toBeTruthy();
     });
 
-    it("should display correct result with error status", () => {
-      jest.spyOn(glob, "sync").mockReturnValueOnce(["test.spec.toml"]);
-      jest.spyOn(fs, "readFileSync").mockReturnValueOnce(`
+    it('should display correct result with error status', () => {
+      jest.spyOn(glob, 'sync').mockReturnValueOnce(['test.spec.toml']);
+      jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(`
           stdout="Failing test"
           args="argument_1 argument_2 argument_3"
           description="This is a failing test"
         `);
       try {
         new TestRunner().run();
-      } catch (e: any) {
-        expect(e.message).toEqual("toml-it silently failed.");
+      } catch (e) {
+        expect((e as { message: string }).message).toEqual(
+          'toml-it silently failed.'
+        );
       }
     });
   });
--- src/models/structure.model.ts
+++ src/models/structure.model.ts
@@ -6,11 +6,15 @@ export class Structure {
   args: string;
   filename: string;
 
-  private checkProperties(o: object): boolean {
+  private checkProperties(o: {
+    stdout: string;
+    description: string;
+    args: string;
+  }): boolean {
     if (
-      (o as any).stdout === undefined ||
-      (o as any).description === undefined ||
-      (o as any).args === undefined
+      o.stdout === undefined ||
+      o.description === undefined ||
+      o.args === undefined
     ) {
       throw new Error('Properties are probably mispelled.');
     }
@@ -24,16 +28,23 @@ export class Structure {
     return true;
   }
 
-  private validate(o: object) {
+  private validate(o: { stdout: string; description: string; args: string }) {
     this.checkProperties(o);
     this.checkPropertiesLength(o);
   }
 
-  constructor(o: object, filename: string) {
+  constructor(
+    o: {
+      stdout: string;
+      description: string;
+      args: string;
+    },
+    filename: string
+  ) {
     this.validate(o);
     this.filename = filename;
-    this.stdout = (o as any).stdout;
-    this.args = (o as any).args;
-    this.description = (o as any).description;
+    this.stdout = o.stdout;
+    this.args = o.args;
+    this.description = o.description;
   }
 }
